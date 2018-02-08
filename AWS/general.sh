e81844635d85037b03f916c87ace17a0b6e65e1f
Luisa Santus <luisa.santus95@gmail.com>
2018-02-08 14:15:44 +0100

    Update general.sh

--- AWS/general.sh
+++ AWS/general.sh
@@ -5,3 +5,18 @@ ssh -i key.pem ec2-user@$nameOfMachine
 #copy to machine
 scp  -i key.pem -r $file  ec2-user@$machine
 
+#find id of processes running in the background
+jobs
+(for processes running by the current shell) 
+ps -aux 
+(all processes from other shells too)
+
+#kill a process
+kill $process
+
+#how much memory 
+df -h
+
+#Run a process in the background
+nohup bash myScript.sh myParam &
+
